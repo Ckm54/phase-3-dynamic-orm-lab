8340a56035cd9721e0cb34d0ea0d8510616a465c
Collins <mutaic84@gmail.com>
2022-06-16 08:16:23 +0300

    commit tree ea3aa97bc6dba5ee35873222584ca47e5d2b0f82

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,5 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require "pry"
 
 class InteractiveRecord
 
@@ -39,6 +40,25 @@ class InteractiveRecord
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
     values.join(", ")
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    DB[:conn].execute(sql, name)
+  end
+
+  def self.find_by(attributes)
+    key = attributes.keys.first
+    value = attributes.values.first
+    formatted = value.class == Integer ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{key} = #{formatted}"
+    DB[:conn].execute(sql)
   end
   
 end
